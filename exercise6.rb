a78d4dedeaf8a1377de191f132bb2cb6333e3187
ndoak <natalieannedoak@gmail.com>
2016-01-13 15:38:46 -0500

    delete salmon from array using .delete_at

--- exercise6.rb
+++ exercise6.rb
@@ -1,24 +1,24 @@
 #create array of grocery items
-grocery_list = ["cheese", "milk", "bread", "chicken", "tomatoes","butter"]
+grocery_list = ["cheese", "milk", "bread", "chicken", "tomatoes","butter", "salmon"]
 
 # grocery_list.each do |x|
 #   puts "*" + x
 # end
 
 # add rice to grocery list
-grocery_list << ("rice")
+# grocery_list << ("rice")
 #
 # grocery_list.each do |x|
 #   puts "*" + x
 # end
 
 #create a method that will output the list with less code
-# def grocery(item)
-#   item. each do |x|
-#     puts "*" + x
-#   end
-# end
-# grocery(grocery_list)
+def grocery(item)
+  item. each do |x|
+    puts "*" + x
+  end
+end
+grocery(grocery_list)
 
 #Print total number of items on your list
 puts grocery_list.length
@@ -32,4 +32,11 @@ end
 bananas(grocery_list)
 
 #second item in the list
-puts grocery_list[1]
+# puts grocery_list[1]
+
+#sort your list alphabetically
+grocery (grocery_list.sort)
+
+#delete item from array using specific index #
+grocery_list.delete_at(6)
+puts grocery_list
