5af10dc9af6f9d3df19e189a5551d06a545eee55
ndoak <natalieannedoak@gmail.com>
2016-01-13 17:08:30 -0500

    Bonus question

--- exercise7.rb
+++ exercise7.rb
@@ -3,7 +3,7 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22,
 }
-
+#create method to display hash
 def info(hash)
   hash.each do |key, value|
   puts "#{key} : #{value} students "
@@ -15,8 +15,17 @@ info (students)
 students [:cohort4] = 43
 puts students
 
-
+#output only keys
 puts students.keys
 
+#increase students by 5%
 increase = students.each { | key , value| students[key]=(value * 0.05) + value }
 info(increase)
+
+#delete cohort2 and redisplay hash
+students.delete(:cohort2)
+info(students)
+
+#BONUS: puts sum of all students 
+my_new_val = students.values.inject(0) { |sum, value|  sum + value }
+puts my_new_val
