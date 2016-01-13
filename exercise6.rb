b59a8aa9ad843488e3bf0d34717d2a8b4196f2dd
ndoak <natalieannedoak@gmail.com>
2016-01-13 14:58:55 -0500

    output total number of items on grocery list array

--- exercise6.rb
+++ exercise6.rb
@@ -1,6 +1,24 @@
 #create array of grocery items
 grocery_list = ["cheese", "milk", "bread", "chicken", "tomatoes","butter"]
 
-grocery_list.each do |x|
-  puts "*" + x
+# grocery_list.each do |x|
+#   puts "*" + x
+# end
+
+# add rice to grocery list
+grocery_list << ("rice")
+#
+# grocery_list.each do |x|
+#   puts "*" + x
+# end
+
+#create a method that will output the list with less code
+def grocery(item)
+  item. each do |x|
+    puts "*" + x
+  end
 end
+grocery(grocery_list)
+
+#Print total number of items on your list
+puts grocery_list.length
